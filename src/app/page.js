d2f4286a8d3179ff6355d216fd7a35129c070bdd
Michel Lima <michelpl@yahoo.com.br>
2025-01-14 10:44:27 -0300

    Install next.js and MUI

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main>
+        <h1>OLÁA</h1>
+      </main>
+    </div>
+  );
+}
